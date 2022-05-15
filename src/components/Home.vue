cbc387b9b67700c9b4a58b4d6f59bf1ff1a8fdd8
Valerian <valerianbaillet@gmail.com>
2022-05-15 17:24:16 +0200

    Ajout du contenu dans la page home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,74 @@
 <template>
-  Coucou
+  <h1>Bienvenue !</h1>
+
+  <p>Je m'appelle Valérian, j'ai 27 ans et je suis développeur spécialisé en ingénieurie des systèmes d'informations. <br/>
+  Ce projet à pour but de me présenter à vous, et pour moi de découvrir le monde fabuleux de Vue3.</p>
+
+  <p>Sur ce site, vous allez découvrir mes compétences, mon parcours et mes futures projets. N'hésitez pas à me contacter !</p>
+
+  <h2>Mes qualités</h2>
+  <a-row>
+  <a-col :span="8"><area-chart-outlined :style="{fontSize : '32px', color:'blue'}"/> 
+      <br/> Curieux 
+      <br/> J'adore découvrir de nouvelles technologies et de nouvelles façons de travailler. Les challenges et les recherches me passionnent.
+  </a-col>
+  <a-col :span="8"><area-chart-outlined :style="{fontSize : '32px', color:'blue'}"/> 
+      <br/> Travailleur
+      <br/> Je n'hésite pas à travailler pour arriver à un résultat qui me satisfait. Je tiens également mes engagements. 
+      </a-col> 
+  <a-col :span="8"><area-chart-outlined :style="{fontSize : '32px', color:'blue'}"/> 
+      <br/> Coopératif
+      <br/> Le travail en équipe me permet d'évoluer et de continuer à apprendre de nouvelles choses. On est toujours meilleurs en équipe !.
+  </a-col>
+  </a-row>
+
+  <h2>Mes technologies</h2>
+  <a-progress v-for="technology in technologies" :key="technology.name" type="circle" :percent="technology.percent" :format="() => technology.name"></a-progress>
 </template>
 
+
+
 <script>
+
+import { AreaChartOutlined } from '@ant-design/icons-vue';
+
 export default {
-  name: "Home"
+  name: "Home",
+  components :{
+    AreaChartOutlined
+  }, 
+  data : () => ({
+    technologies : [
+      {
+        percent : 75,
+        name : 'java'
+      },
+      {
+        percent : 70,
+        name : 'spring'
+      },
+      {
+        percent : 40,
+        name : 'python'
+      },
+      {
+        percent : 70,
+        name : 'typescript'
+      },
+      {
+        percent : 70,
+        name : 'HTML'
+      },
+      {
+        percent : 55,
+        name : 'CSS'
+      },
+      {
+        percent : 70,
+        name : 'javascript'
+      }
+      ],
+  })
 }
 </script>
 
